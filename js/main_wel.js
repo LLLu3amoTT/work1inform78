9f47159f486968a50f005d040894819d9cbcdaae
LLLu3amoTT <92044108+LLLu3amoTT@users.noreply.github.com>
2022-12-14 18:28:26 +0200

    Add files via upload

---
+++ js/main_wel.js
@@ -0,0 +1,37 @@
+let button_1 = document.getElementById('p1')
+let button_2 = document.getElementById('p2')
+let button_3 = document.getElementById('p3')
+let button_4 = document.getElementById('p4')
+let button_5 = document.getElementById('p5')
+let button_6 = document.getElementById('p6')
+
+button_1.addEventListener('click',fun1)
+button_2.addEventListener('click',fun2)
+button_3.addEventListener('click',fun3)
+button_4.addEventListener('click',fun4)
+button_5.addEventListener('click',fun5)
+button_6.addEventListener('click',fun6)
+
+function fun1() {
+    window.location = ('/sites/index.html')
+}
+
+function fun2() {
+    window.location = ('/sites/index_2.html')
+}
+
+function fun3() {
+    window.location = ('/sites/index_3.html')
+}
+
+function fun4() {
+    window.location = ('/sites/index_4.html')
+}
+
+function fun5() {
+    window.location = ('/sites/index_5.html')
+}
+
+function fun6() {
+    window.location = ('/sites/index_6.html')
+}
